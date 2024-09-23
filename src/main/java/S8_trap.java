8986eb9ee6ad03a9917bd9c184bf6c1f2e04727a
psxjoy <psxjoy@outlook.com>
2024-09-23 20:58:41 +0800

    add S6/S7/S8

--- src/main/java/S8_trap.java
+++ src/main/java/S8_trap.java
@@ -21,6 +21,22 @@ public class S8_trap {
     }
 
     public int trap2(int[] height) {
-
+        int ans = 0;
+        int left =0;
+        int right = height.length-1;
+        int preMax = 0;
+        int sufMax =0;
+        while(left<=right) {
+            preMax =Math.max(preMax,height[left]);
+            sufMax = Math.max(sufMax,height[right]);
+            if(preMax<sufMax) {
+                ans +=preMax-height[left];
+                left++;
+            }else {
+                ans +=sufMax-height[right];
+                right--;
+            }
+        }
+        return ans;
     }
 }
