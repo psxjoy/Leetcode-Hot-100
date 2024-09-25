a0b4d17228b4087c20b7db224aa43509a0ba02a8
Black Pan <psxjoy@outlook.com>
2024-09-25 22:01:20 +0800

    add 4-day tasks

--- src/main/java/S9_lengthOfLongestSubstring.java
+++ src/main/java/S9_lengthOfLongestSubstring.java
@@ -1,9 +1,28 @@
-public class S9_lengthOfLongestSubstring {
-    public int lengthOfLongestSubstring(String s) {
-        int left = 0;
-        int right = s.length();
-        int ans = 0;
-        return ans;
-    }
+import java.util.HashMap;
+import java.util.Map;
 
+public class S9_lengthOfLongestSubstring {
+    
+    /**
+     * 给定一个字符串 s ，请你找出其中不含有重复字符的 最长 子串 的长度。
+     */
+    public int lengthOfLongestSubstring(String s) {
+        Map<Character, Integer> window = new HashMap<>();
+        int left = 0;
+        int right = 0;
+        int res = 0;
+        while (right < s.length()) {
+            char c = s.charAt(right);
+            window.put(c, window.getOrDefault(c, 0) + 1);
+            right++;
+            while (window.get(c) > 1) {
+                char d = s.charAt(left);
+                window.put(d, window.get(d) - 1);
+                left++;
+            }
+            res = Math.max(res, right - left);
+        }
+        return res;
+    }
+    
 }
