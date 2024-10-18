ad82dc94934b56040aabcfbf5e66c8cb778a0fee
psxjoy <psxjoy@outlook.com>
2024-10-18 17:25:00 +0800

    add S13

---
+++ src/main/java/S13_maxSlidingWindow.java
@@ -0,0 +1,25 @@
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class S13_maxSlidingWindow {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        if (nums.length == 0 || k == 0) {
+            return new int[0];
+        }
+        Deque<Integer> deque = new LinkedList<>();
+        int[] res = new int[nums.length - k + 1];
+        for (int j = 0, i = 1 - k; j < nums.length; j++) {
+            if (i > 0 && deque.peekFirst() == nums[i - 1]) {
+                deque.removeFirst();
+            }
+            while (!deque.isEmpty() && deque.peekLast() < nums[j]) {
+                deque.removeFirst();
+            }
+            deque.addLast(nums[j]);
+            if (i >= 0) {
+                res[i] = deque.peekFirst();
+            }
+        }
+        return res;
+    }
+}
