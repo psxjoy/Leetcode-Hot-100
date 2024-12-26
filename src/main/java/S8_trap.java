c007cb3e11f93b12840278b8ff64c63094930754
psxjoy <psxjoy@outlook.com>
2024-12-26 20:55:23 +0800

    add S13

--- src/main/java/S8_trap.java
+++ src/main/java/S8_trap.java
@@ -22,18 +22,18 @@ public class S8_trap {
 
     public int trap2(int[] height) {
         int ans = 0;
-        int left =0;
-        int right = height.length-1;
+        int left = 0;
+        int right = height.length - 1;
         int preMax = 0;
-        int sufMax =0;
-        while(left<=right) {
-            preMax =Math.max(preMax,height[left]);
-            sufMax = Math.max(sufMax,height[right]);
-            if(preMax<sufMax) {
-                ans +=preMax-height[left];
+        int sufMax = 0;
+        while (left <= right) {
+            preMax = Math.max(preMax, height[left]);
+            sufMax = Math.max(sufMax, height[right]);
+            if (preMax < sufMax) {
+                ans += preMax - height[left];
                 left++;
-            }else {
-                ans +=sufMax-height[right];
+            } else {
+                ans += sufMax - height[right];
                 right--;
             }
         }
